16fab25176623bc071a847e02c811dd51be42e1d
ibra-exe <ibrahim_x@msn.com>
2021-01-04 12:05:52 +0300

    login/logout is functional

--- controllers/courseCon.js
+++ controllers/courseCon.js
@@ -6,13 +6,42 @@ const session = require("express-session");
 const config = require("dotenv").config();
 const mongoSessisonStore = require("connect-mongo")(session);
 const validator = require("express-validator");
+const Course = require("../models/courseModel");
+
+var bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
+
+
+router.use(
+    session({
+        store: new mongoSessisonStore({ mongooseConnection: mongoose.connection }),
+        saveUninitialized: true,
+        resave: true,
+        secret: "SuperSecretCookie",
+        cookie: { maxAge: 30 * 60 * 1000 },
+    })
+);
+
+//***************************** Routes *****************************//
+
+router.get('/courses', (req, res) => {
+
+    res.render("courses/courses", {userId: req.session.userId});
+
+});
+
+router.get('/courses/:id', (req, res) => {
+
+    
+    
+    res.render("courses/courses", {userId: req.session.userId});
+
+});
 
 
 
 
 
 
-
-
-// Export
+//***************************** Export *****************************//
 module.exports = router;
